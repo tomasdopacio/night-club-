5758aa8cdf3d46e122515db24427dc1670c6ee59
Matias <matiaskornuta@gmail.com>
2024-06-30 20:19:06 -0300

    modifico el models.py

--- backend/models.py
+++ backend/models.py
@@ -30,7 +30,9 @@ class Combos(db.Model):
     __tablename = "combos"
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(255), nullable = False)
+    descripcion = db.Column(db.String(255), nullable = False)
     imagen = db.Column(db.String(255), nullable = False)
+    precio = db.Column(db.Integer, nullable = False)
     id_comida = db.Column(db.Integer, db.ForeignKey("comidas.id"))
     id_bebida = db.Column(db.Integer, db.ForeignKey("bebidas.id"))
     id_tragos = db.Column(db.Integer, db.ForeignKey("tragos.id"))
