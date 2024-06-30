d49f3d6a603ad41813ae19a555382f4f08bcbabc
Matias <matiaskornuta@gmail.com>
2024-06-30 18:14:33 -0300

    corrigo errores de sintaxis en 'models.py' e implemento sqlAlchemy a 'app.py'

--- backend/models.py
+++ backend/models.py
@@ -4,35 +4,35 @@ db = SQLAlchemy()
 
 class Comidas(db.Model):
     tablename = "comidas"
-    id = db.Column(db.Inegrer, primarykey = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255), nullable = False)
     descripcion = db.Column(db.String(255), nullable = False)
     imagen = db.Column(db.String(255), nullable = False)
-    precio = db.Column(db.Integrer, nullable = False)
+    precio = db.Column(db.Integer, nullable = False)
 
 class Bebidas(db.Model):
     tablename = "bebidas"
-    id = db.Column(db.Integrer, primarykey = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255), nullable = False)
     descripcion = db.Column(db.String(255), nullable = False)
     imagen = db.Column(db.String(255), nullable = False)
-    precio = db.Column(db.Integrer, nullable = False)
+    precio = db.Column(db.Integer, nullable = False)
 
 class Tragos(db.Model):
     _tablename = "tragos"
-    id = db.Column(db.Integrer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255), nullable = False)
     descripcion = db.Column(db.String(255), nullable = False)
     imagen = db.Column(db.String(255), nullable = False)
-    precio = db.Column(db.Integrer, nullable = False)
+    precio = db.Column(db.Integer, nullable = False)
 
 class Combos(db.Model):
     __tablename = "combos"
-    id = db.Column(db.Inegrer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255), nullable = False)
     descripcion = db.Column(db.String(255), nullable = False)
     imagen = db.Column(db.String(255), nullable = False)
-    precio = db.Column(db.Integrer, nullable = False)
-    id_comida = db.Column(db.Integrer, db.ForeingKey("comidas.id"))
-    id_bebida = db.Column(db.Integrer, db.ForeingKey("bebidas.id"))
-    id_tragos = db.Column(db.Integrer, db.ForeingKey("tragos.id"))
+    precio = db.Column(db.Integer, nullable = False)
+    id_comida = db.Column(db.Integer, db.ForeignKey("comidas.id"))
+    id_bebida = db.Column(db.Integer, db.ForeignKey("bebidas.id"))
+    id_tragos = db.Column(db.Integer, db.ForeignKey("tragos.id"))
